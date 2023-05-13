3774f0a88e9ac68979bd7afc630d8b067b26c5a5
nhatlinh <caonhatlinh1312@gmail.com>
2023-05-14 01:25:01 +0700

    update js

--- assets/js/base.js
+++ assets/js/base.js
@@ -85,35 +85,6 @@ for (let i = 0; i < links.length; i++) {
 
             
         
-    
-const showItem = document.querySelector(".product"),
- SecMotorbike = document.querySelectorAll(".sec__item"),
- motorbike = document.querySelectorAll(".sec__item a"),
- productList = document.querySelectorAll(".product__list");
-console.log(motorbike);
-console.log(SecMotorbike);
-SecMotorbike.forEach((item, index, arr)=> {
-    SecMotorbike[index].addEventListener("click",()=>{
-        
-        for(let i = 0; i <= motorbike.length; i++)
-        {
-            if (index == i)
-            {
-                SecMotorbike[i].classList.add("active");
-                productList[i].classList.add("show-item");
-            }
-            else 
-            {
-                SecMotorbike[i].classList.remove("active");
-                productList[i].classList.remove("show-item");
-
-
-            }
-
-        }
-    })
-    
-})
 
 
 
