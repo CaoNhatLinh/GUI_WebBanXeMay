267dab18fce260a41eed8561a7b85270fd76a0c7
Minh Tuyen <bachminhtuyen2003@gmail.com>
2023-05-27 02:09:40 +0700

    Insert responsive for payment

--- assets/js/payment.js
+++ assets/js/payment.js
@@ -15,18 +15,18 @@ for(var i = 0; i < addressList.length; i++){
     addressList[i].onchange = function(e){
         console.log(e.target.value)
         if(e.target.value != null){
-            this.options[0].style.display = 'none'
+            this.options[0].style.display = 'none';
         }
         //Gán phí vận chuyển
         if(e.target.value == '50'){
-            transportFee = 300000
-            nameMethod[1].innerHTML = transportFee.toLocaleString()+'₫'
-            sum.innerHTML = tinhTong().toLocaleString()
+            transportFee = 300000;
+            nameMethod[1].innerHTML = transportFee.toLocaleString()+'₫';
+            sum.innerHTML = tinhTong().toLocaleString();
         }
         else{
-            transportFee = 700000
-            nameMethod[1].innerHTML = transportFee.toLocaleString()+'₫'
-            sum.innerHTML = tinhTong().toLocaleString()
+            transportFee = 700000;
+            nameMethod[1].innerHTML = transportFee.toLocaleString()+'₫';
+            sum.innerHTML = tinhTong().toLocaleString();
         }
     }
 }
@@ -35,15 +35,15 @@ for(var i = 0; i < addressList.length; i++){
 var deliveryMethodList = $$('.delivery-method')
 
 // Phương thức giao hàng
-// deliveryMethodList[0].onchange = function(e){
+deliveryMethodList[0].onchange = function(e){
 //     console.log("a");
 //     deliveryMethodList[0].querySelector('input[type="radio"]').checked = true;
 //     deliveryMethodList[1].querySelector('input[type="radio"]').checked = false;
 
     if(e.target.checked == true){
-        $('.receive').style.display = 'none'
-        $('.showroom').style.display = 'none'
-        $('.delivery').style.display = 'block'
+        // $('.receive').style.display = 'none'
+        // $('.showroom').style.display = 'none'
+        // $('.delivery').style.display = 'block'
         transportFee = temp
         nameMethod[1].innerHTML = transportFee.toLocaleString() + '₫'
         nameMethod[2].innerHTML = this.querySelector('label').innerHTML;
@@ -52,17 +52,17 @@ var deliveryMethodList = $$('.delivery-method')
 }
 
 //Phương thức nhận tại cửa hàng
-// deliveryMethodList[1].onchange = function(e){
-//     console.log("a");
+deliveryMethodList[1].onchange = function(e){
+    console.log("a");
 
-    deliveryMethodList[1].querySelector('input[type="radio"]').checked = true;
-    deliveryMethodList[0].querySelector('input[type="radio"]').checked = false;
+    // deliveryMethodList[1].querySelector('input[type="radio"]').checked = true;
+    // deliveryMethodList[0].querySelector('input[type="radio"]').checked = false;
     if(e.target.checked == true){
         temp = transportFee
         transportFee = 0
-        $('.receive').style.display = 'flex'
-        $('.showroom').style.display = 'block'
-        $('.delivery').style.display = 'none'
+        // $('.receive').style.display = 'flex'
+        // $('.showroom').style.display = 'block'
+        // $('.delivery').style.display = 'none'
         nameMethod[1].innerHTML = transportFee.toLocaleString() + '₫';
         nameMethod[2].innerHTML = this.querySelector('label').innerHTML
         sum.innerHTML = tinhTong().toLocaleString()
@@ -74,12 +74,12 @@ var payMethod = $$('.pay-method')
 
 payMethod[0].onchange = function(e){
     if(e.target.checked == true){
-        nameMethod[3].innerHTML = this.querySelector('span').innerHTML
+        nameMethod[3].innerHTML = this.querySelector('label').innerHTML
     }
 }
 payMethod[1].onchange = function(e){
     if(e.target.checked == true){
-        nameMethod[3].innerHTML = this.querySelector('span').innerHTML
+        nameMethod[3].innerHTML = this.querySelector('label').innerHTML
     }
 }
 
