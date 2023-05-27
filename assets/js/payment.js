1151f904533fc6abd644a79eb5d4b0bc581ead2a
nhatlinh <caonhatlinh1312@gmail.com>
2023-05-28 00:12:06 +0700

    fix js

--- assets/js/payment.js
+++ assets/js/payment.js
@@ -1,4 +1,85 @@
 //TODO: Get Provinces API
+
+
+
+
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+//TODO: Thông tin thanh toán
+var nameMethod = $$('.money span')
+
+//TODO: Lựa chọn tỉnh/ thành phố để tính phí vận chuyển (mặc định)
+var addressList = $$('.address-select select')
+
+// $('.receive').style.display = 'none'
+// $('.showroom').style.display = 'none'
+
+var transportFee = 0;
+var temp;
+for(var i = 0; i < addressList.length; i++){
+    addressList[i].onchange = function(e){
+       
+        if(e.target.value == '50'){
+            transportFee = 300000;
+        }
+        else{
+            transportFee = 700000;
+        }
+        nameMethod[1].innerHTML = convert(transportFee);
+        sum.innerHTML = convert(tinhTong());
+    }
+}
+
+// TODO: Lựa chọn phương thức vận chuyển
+var deliveryMethodList = $$('.delivery-method')
+
+// Phương thức giao hàng
+deliveryMethodList[0].onchange = function(e){
+
+    if(e.target.checked == true){
+        transportFee = temp
+        nameMethod[1].innerHTML = convert(transportFee)
+        nameMethod[2].innerHTML = this.querySelector('label').innerHTML;
+        sum.innerHTML = convert(tinhTong());
+    }
+}
+
+//Phương thức nhận tại cửa hàng
+deliveryMethodList[1].onchange = function(e){
+    if(e.target.checked == true){
+        temp = transportFee;
+        transportFee = 0;
+        nameMethod[1].innerHTML = convert(transportFee)
+        nameMethod[2].innerHTML = this.querySelector('label').innerHTML
+        sum.innerHTML = convert(tinhTong());
+    }
+}
+
+//TODO: Phương thức thanh toán
+var payMethod = $$('.pay-method')
+
+payMethod[0].onchange = function(e){
+    if(e.target.checked == true){
+        nameMethod[3].innerHTML = this.querySelector('label').innerHTML
+    }
+}
+payMethod[1].onchange = function(e){
+    if(e.target.checked == true){
+        nameMethod[3].innerHTML = this.querySelector('label').innerHTML
+    }
+}
+
+//Todo: Tính tổng tiền
+var sum = $('.total .number span');
+var quantily = $('.payment-info__quantily').innerText;
+var price = 73290000;
+function tinhTong(){
+    return quantily * 73290000 + transportFee;
+}
+sum.innerHTML = convert(tinhTong())
+const priceShow = $$('.payment-info .price');
+priceShow[0].innerHTML = convert(price);
 var getProvincesApi = 'https://provinces.open-api.vn/api/';
 
 var provinceSelect = document.getElementById('provinceSelect');
@@ -16,6 +97,7 @@ fetch(getProvincesApi)
             provinceSelect.add(option);
     })
 })
+
 
 
 var getDistrictsApi = 'https://provinces.open-api.vn/api/d/';
@@ -48,89 +130,16 @@ fetch(getDistrictsApi)
         })
 })
 
-const $ = document.querySelector.bind(document)
-const $$ = document.querySelectorAll.bind(document)
 
-//TODO: Thông tin thanh toán
-var nameMethod = $$('.money span')
 
-//TODO: Lựa chọn tỉnh/ thành phố để tính phí vận chuyển (mặc định)
-var addressList = $$('.address-select select')
-
-// $('.receive').style.display = 'none'
-// $('.showroom').style.display = 'none'
-
-var transportFee = 0;
-var temp;
-for(var i = 0; i < addressList.length; i++){
-    addressList[i].onchange = function(e){
-        console.log(e.target.value)
-        if(e.target.value != null){
-            this.options[0].style.display = 'none';
-        }
-        //Gán phí vận chuyển
-        if(e.target.value == '50'){
-            transportFee = 300000;
-            nameMethod[1].innerHTML = transportFee.toLocaleString()+'₫';
-            sum.innerHTML = tinhTong().toLocaleString();
-        }
-        else{
-            transportFee = 700000;
-            nameMethod[1].innerHTML = transportFee.toLocaleString()+'₫';
-            sum.innerHTML = tinhTong().toLocaleString();
-        }
-    }
+function convert(number) {
+    let vnd = number.toLocaleString('vi', { style: 'currency', currency: 'VND' });
+    vnd = vnd.replace('.', ',');
+    return vnd;
 }
 
-// TODO: Lựa chọn phương thức vận chuyển
-var deliveryMethodList = $$('.delivery-method')
 
-// Phương thức giao hàng
-deliveryMethodList[0].onchange = function(e){
 
-    if(e.target.checked == true){
-        transportFee = temp
-        nameMethod[1].innerHTML = transportFee.toLocaleString() + '₫'
-        nameMethod[2].innerHTML = this.querySelector('label').innerHTML;
-        sum.innerHTML = tinhTong().toLocaleString()
-    }
-}
 
-//Phương thức nhận tại cửa hàng
-deliveryMethodList[1].onchange = function(e){
-    if(e.target.checked == true){
-        temp = transportFee;
-        transportFee = 0;
-        nameMethod[1].innerHTML = transportFee.toLocaleString() + '₫';
-        nameMethod[2].innerHTML = this.querySelector('label').innerHTML
-        sum.innerHTML = tinhTong().toLocaleString()
-    }
-}
 
-//TODO: Phương thức thanh toán
-var payMethod = $$('.pay-method')
-
-payMethod[0].onchange = function(e){
-    if(e.target.checked == true){
-        nameMethod[3].innerHTML = this.querySelector('label').innerHTML
-    }
-}
-payMethod[1].onchange = function(e){
-    if(e.target.checked == true){
-        nameMethod[3].innerHTML = this.querySelector('label').innerHTML
-    }
-}
-
-//Todo: Tính tổng tiền
-var sum = $('.total .number span');
-var quantily = $('.payment-info__quantily').innerText;
-var price = 73290000;
-
-var priceShow = $('.payment-info .price')
-priceShow.innerHTML = price.toLocaleString() + '₫'
-
-function tinhTong(){
-    return quantily * 73290000 + transportFee;
-}
-sum.innerHTML = tinhTong().toLocaleString()
 
