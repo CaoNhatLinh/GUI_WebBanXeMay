0ba179eae2335e62d07763f519dcb2d6357fbbe2
nhatlinh <caonhatlinh1312@gmail.com>
2023-05-28 16:02:40 +0700

    a

---
+++ assets/js/list-product-admin.js
@@ -0,0 +1,157 @@
+let dataMotorbike = {
+    xeTayGa: [
+        {
+           
+            name: "Air Blade 125",
+            subName: "Xe tay ga",
+            price: 150990000,
+            img: "https://phattien.com/wp-content/uploads/2020/01/20220524-air-blade-125-chi-tiet-xe-tieu-chuan-xanh-den.png",
+            tag: 1,
+            link: "./san-pham-admin/product-detail-4.html"
+        },
+        {
+            
+            name: "Air Blade 160",
+            subName: "Xe tay ga",
+            price: 2500000,
+            img: "https://phattien.com/wp-content/uploads/2020/01/ab.png",
+            tag: 1,
+            link: "./san-pham-admin/product-detail-3.html"
+        },
+        {
+           
+            name: "Honda LEAD 125",
+            subName: "Xe tay ga",
+            price: 56490000,
+            img: "https://phattien.com/wp-content/uploads/2019/10/lead-550x400.png",
+            tag: 1,
+            link: "./san-pham-admin/product-detail-1.html"
+        },
+        {
+           
+            name: "Honda SH125i",
+            subName: "Xe tay ga",
+            price: 56490000,
+            img: "https://phattien.com/wp-content/uploads/2019/10/sh125i-xam-den-removebg-preview.png",
+            tag: 1,
+            link: "./san-pham-admin/product-detail-2.html"
+        },
+        {
+            name: "SH 125i/160i",
+            subName: "Xe tay ga",
+            price: 56490000,
+            img: "https://phattien.com/wp-content/uploads/2019/10/sh160.png",
+            tag: 1,
+            link: "./san-pham-admin/product-detail-4.html"
+        },
+        {
+            
+            name: "Honda SH350i",
+            subName: "Xe tay ga",
+            price:56490000,
+            img: "https://phattien.com/wp-content/uploads/2019/10/SH350i-ban-dac-biet-bac-den.png",
+            tag: 1,
+            link: "./san-pham-admin/product-detail-1.html"
+        },
+
+        {
+          
+            name: "Honda VARIO 160",
+            subName: "Xe tay ga",
+            price: 56490000,
+            img: "https://phattien.com/wp-content/uploads/2022/12/vario-the-thao-xam-den-bac1-e1672364326339-550x400.png",
+            tag: 1,
+            link: "./san-pham-admin/product-detail-3.html"
+        },
+       
+        {
+            
+            name: "Sh Mode 125",
+            subName: "Xe tay ga",
+            price: 56490000,
+            img: "https://phattien.com/wp-content/uploads/2020/07/gf8B4s14htSXebW5PrFy-1-768x558.png",
+            tag: 1,
+            link: "./san-pham-admin/product-detail-2.html"
+        }
+
+    ],
+
+    xeSo: [
+        {
+            
+            name: "Honda BLADE 110",
+            subName: "Xe Số",
+            price: 150990000,
+            img: "https://phattien.com/wp-content/uploads/2019/10/blade-dac-biet-den.png",
+            tag: 2,
+            link: "./san-pham-admin/product-detail-4.html"
+        },
+        {
+            
+            name: "Wave ALPHA 110",
+            subName: "Xe Số",
+            price: 2500000,
+            img: "https://phattien.com/wp-content/uploads/2019/10/20220823-wave-alpha-2023-chi-tiet-xe-dac-biet-den-mo.png",
+            tag: 2,
+            link: "./san-pham-admin/product-detail-3.html"
+        },
+        {
+            
+            name: "Wave RSX FI",
+            subName: "Xe Số",
+            price: 56490000,
+            img: "https://phattien.com/wp-content/uploads/2019/10/wave-den-phanh-dia.png",
+            tag: 2,
+            link: "./san-pham-admin/product-detail-1.html"
+        },
+        {
+            
+            name: "Honda FUTURE",
+            subName: "Xe Số",
+            price: 56490000,
+            img: "https://phattien.com/wp-content/uploads/2019/10/Fu-dac-biet-xanh-den.png",
+            tag: 2,
+            link: "./san-pham-admin/product-detail-2.html"
+        },
+        {
+
+            name: "SUPER CUB C125",
+            subName: "Xe Số",
+            price:54490000,
+            img: "https://phattien.com/wp-content/uploads/2019/10/20211025-Super-Cub-C125-chi-tiet-xe-ban-tieu-chuan-xanh-trang.png",
+            tag: 2,
+            link: "./san-pham-admin/product-detail-4.html"
+        }
+    ],
+
+    xeConTay: [
+        {
+            id: 1,
+            name: "Honda CB150R",
+            subName: "Xe Côn Tay",
+            price: 150990000,
+            img: "https://phattien.com/wp-content/uploads/2019/10/20220524-cb150r-chi-tiet-xe-tieu-chuan-den.png",
+            tag: 3,
+            link: "./san-pham-admin/product-detail-3.html"
+        },
+        {
+            id: 2,
+            name: "Honda WINNER X",
+            subName: "Xe Côn Tay",
+            price: 2500000,
+            img: "https://phattien.com/wp-content/uploads/2021/10/11102021-cbr150r-chi-tiet-xe-den-xam.png",
+            tag: 3,
+            link: "./san-pham-admin/product-detail-2.html"
+        },
+        {
+            id: 3,
+            name: "Honda WINNER X",
+            subName: "Xe Côn Tay",
+            price: 56490000,
+            img: "https://phattien.com/wp-content/uploads/2019/10/20211224-winner-x-chitietxe-dacbiet-dodenxanh.png",
+            tag: 3,
+            link: "./san-pham-admin/product-detail-1.html"
+        },
+        
+    ]
+}
